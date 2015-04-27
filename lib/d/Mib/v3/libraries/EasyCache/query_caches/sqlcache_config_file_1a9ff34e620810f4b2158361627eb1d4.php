4ecf740a0cab96a0e3fdafa5f47338c28d01800f
shampeak <ss012003>
2015-04-27 15:58:23 +0800

    0.004

---
+++ lib/d/Mib/v3/libraries/EasyCache/query_caches/sqlcache_config_file_1a9ff34e620810f4b2158361627eb1d4.php
@@ -0,0 +1,6 @@
+<?php
+$this->mysql_config_cache_file_time = 1429177186.5313;
+$this->timeline = 0;
+$this->timezone = 0;
+$this->platform = 'WINDOWS';
+?>
---
+++ lib/d/Mib/v3/libraries/EasyCache/query_caches/sqlcache_config_file_a88f6b2ed762e0d58851785da11a911e.php
@@ -0,0 +1,6 @@
+<?php
+$this->mysql_config_cache_file_time = 1429177759.6187;
+$this->timeline = 0;
+$this->timezone = 0;
+$this->platform = 'WINDOWS';
+?>
